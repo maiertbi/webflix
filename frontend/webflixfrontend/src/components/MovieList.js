ef5b5a191f8a957af732bc3e722b0f81691b4e03
sophieeberle <2010653368@stud.fh-kufstein.ac.at>
2022-11-28 00:24:42 +0000

    implemented searchbox and options

--- frontend/webflixfrontend/src/components/MovieList.js
+++ frontend/webflixfrontend/src/components/MovieList.js
@@ -11,17 +11,36 @@ import RemoveFavourites from './RemoveFavourites.js';
 const MovieList = (props) => {
 	const [movies, setMovies] = useState([]);
 	const [favourites, setFavourites] = useState([]);
+	const [searchKey, setSearchKey] = useState("");
+	const [searchValue, setSearchValue] = useState("");
 
 	useEffect(() => {
-		getMovies();
+		if (searchKey !== "" && searchValue !== ""){
+			getSpecificMovie();	
+		} else {
+			getMovies();
+		}
 	});
 
-
 	const getMovies = async function () { 
-		const response = await axios.get("http://localhost:3000/api/movies/");
-		setMovies(response?.data);
+		try {
+			const response = await axios.get("http://localhost:3000/api/movies/");
+			setMovies(response?.data);
+		} catch (e) {
+			console.error(e);
+		}
 	};
 
+	const getSpecificMovie = async function () {
+		try {
+			const response = await axios.get("http://localhost:3000/api/movies/", {
+				params: {key: searchKey, value: searchValue}
+			});
+			setMovies(response?.data);
+		} catch (e) {
+			console.error(e);
+		}	
+	};
 
 	// everything just for favourites from that point on
 	const saveFavourites = (items) => {
@@ -57,10 +76,21 @@ const MovieList = (props) => {
 			<div className="container movie-app">
 				<div className="row d-flex align-items-center mt-4 mb-4">
 				<MovieListHeading heading="Movies" />
-				<SearchBox
-					// searchValue={searchValue}
-					// setSearchValue={setSearchValue}
-				/> 
+				<select className="selectGenre" id="searchKey" onChange={(e) => setSearchKey(e.target.value)}>
+					<option value="">Select SearchKey...</option>
+					<option value="title">Title</option>
+					<option value="director">Director</option>
+					<option value="year">Year</option>
+					<option value="genre">Genre</option>
+                </select>
+				<div className='col col-sm-4'>
+					<input
+						className='form-control'
+						value={searchValue}
+						onChange={(event) => setSearchValue(event.target.value)}
+						placeholder='Type to search...'>
+					</input>
+				</div>
 				</div>
 				<div className="row">
 					{movies.map((movie, id) => (
@@ -72,7 +102,7 @@ const MovieList = (props) => {
 							</div>
 							<div className="moviename">
 								<Link to={"/movies"}>
-									<button>Moviename</button>
+									<button className="movieBtn">{movie.title}</button>
 								</Link>
 							</div>
 						</div>
@@ -93,7 +123,7 @@ const MovieList = (props) => {
 							</div>
 							<div className="moviename">
 								<Link to={"/movies"}>
-									<button>Moviename</button>
+									<button className="movieBtn">{movie.title}</button>
 								</Link>
 							</div>
 						</div>
